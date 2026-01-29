fda795dd88d0d9f1a931b7cbbdc809579fc3da16
abhishek <abhishekpokhrel53@gmail.com>
2026-01-29 16:05:12 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <strong> Web Frontend Project - ITEC 2026</strong>
+      
+    </>
+  )
+}
+
+export default App
