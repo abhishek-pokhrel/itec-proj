5ac96c9e0d7647b9d90f4b385f54b74cd45dd4df
abhishek-pokhrel <abhishekpokhrel53@gmail.com>
2026-03-26 13:09:22 -0400

    updated cors policy

--- server/server.js
+++ server/server.js
@@ -14,8 +14,15 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 // Middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://itec-proj.vercel.app',
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
+  credentials: true
+}))
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
